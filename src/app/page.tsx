b5befed426ee634e73a1b5f445b5b6adec948469
styleai-luke <luke@styleai.io>
2023-12-10 14:11:41 +0900

    feat: maybe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      새로운 신규 기능~~
+      Maybe
     </main>
   );
 }
